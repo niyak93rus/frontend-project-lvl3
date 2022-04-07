b53da8f9624d56b10fe88742d44930b700386d8b
Nikolay Yakovlev <n.yakovlev96@gmail.com>
2022-04-07 12:04:41 +0500

    replace dist with public directory

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -9,7 +9,7 @@ const isProduction = process.env.NODE_ENV == 'production';
 const config = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
   },
   devServer: {
     open: true,
