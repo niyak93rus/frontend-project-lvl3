65dfeb4e506b76f6e259cead88eef0b2e442bc72
Nikolay Yakovlev <n.yakovlev96@gmail.com>
2022-04-18 14:10:30 +0500

    fix errors

--- src/app.js
+++ src/app.js
@@ -50,12 +50,10 @@ const loadPosts = (userUrl, watchedObject, i18n) => {
         watchedObject.mode = 'showFeed';
       })
       .catch((error) => {
-        if (error === 'Error: Network Error') {
-          watchedObject.status = 'invalid';
-          watchedObject.feedback = i18n.t('networkError');
-        }
         watchedObject.status = 'invalid';
-        watchedObject.feedback = i18n.t('invalidRSS');
+        watchedObject.feedback = i18n.t('networkError');
+        // watchedObject.status = 'invalid';
+        // watchedObject.feedback = i18n.t('invalidRSS');
         console.log(error);
       });
   }
