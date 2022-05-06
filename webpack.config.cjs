72206ab9fe0e7dd3f607015ae17b954c5582e7a7
Nikolay Yakovlev <n.yakovlev96@gmail.com>
2022-05-06 14:00:37 +0500

    fix webpack config: delete irrelevant parts

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,14 +1,10 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-extraneous-dependencies */
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const config = {
+module.exports = {
   entry: './src/run.js',
   output: {
     path: path.resolve(__dirname, 'public'),
@@ -17,60 +13,27 @@ const config = {
     open: true,
     host: 'localhost',
   },
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
-  module: {
-    rules: [
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
-      },
-      {
-        test: /\.(sass|css|scss)$/,
-        use: [{
-          // inject CSS to page
-          loader: 'style-loader',
-        }, {
-          // translates CSS into CommonJS modules
-          loader: 'css-loader',
-        }, {
-          // Run postcss actions
-          loader: 'postcss-loader',
-          options: {
-            // `postcssOptions` is needed for postcss 8.x;
-            // if you use postcss 7.x skip the key
-            postcssOptions: {
-              // postcss plugins, can be exported to postcss.config.js
-              plugins() {
-                return [
-                  require('autoprefixer'),
-                ];
-              },
-            },
-          },
-        }, {
-          // compiles Sass to CSS
-          loader: 'sass-loader',
-        }],
-      },
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
-    ],
-  },
-};
-
-module.exports = () => {
-  if (isProduction) {
-    config.mode = 'production';
-    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-  } else {
-    config.mode = 'development';
-  }
-  return config;
 };
